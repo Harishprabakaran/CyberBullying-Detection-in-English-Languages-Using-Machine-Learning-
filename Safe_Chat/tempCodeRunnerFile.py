81045331bd4ec8832ac978283d23621d59884397
Harish <96423026+Harishprabakaran@users.noreply.github.com>
2022-08-25 19:02:13 +0530

    Add files via upload

---
+++ Safe_Chat/tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+import pickle
+model = pickle.load(open(r"C:\Users\selvi\Desktop\Harish ECE\CyberBullying-Detection-in-English-Languages-Using-Machine-Learning-\Safe_Chat\LinearSVC.pkl", 'rb'))
+print(model)
